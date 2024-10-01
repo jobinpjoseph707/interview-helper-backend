fbec2846c38e84fae92baf0912785c87c3ee76b0
jobinpjoseph707 <jobinpjoseph700@gmail.com>
2024-10-01 15:27:08 +0530

    question fetching api

---
+++ intervirew-helper-backend/DTO/QuestionRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace intervirew_helper_backend.DTO
+{
+    public class QuestionRequest
+    {
+        public int CandidateId { get; set; }
+        public List<TechnologyExperience> Technologies { get; set; }
+    }
+
+    public class TechnologyExperience
+    {
+        public int TechnologyId { get; set; }  // ID of the technology
+        public int ExperienceLevelId { get; set; }  // ID of the experience level
+    }
+
+}
